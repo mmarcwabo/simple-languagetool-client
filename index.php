9582df4a568929da07052a2f8ccbcdf81e5cdb52
marcwabo <marcwabo20@gmail.com>
2021-10-16 13:42:27 +0100

    Update views'

--- index.php
+++ index.php
@@ -2,5 +2,14 @@
 //Call the restcall object
 //To be able to send async calls to the api
 
-$rc = new RestCall();
+require 'vendor/autoload.php';
 
+// For this to work properly, the class must be the same as the file
+spl_autoload_register(function ($class) {
+
+    require "app/$class.php";
+});
+
+$rc = new RestCall();
+$v = new View();
+$v->render("views/index.php");
