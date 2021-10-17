a4606de63d27a99d2156c2c4e02498040b766b3e
marcwabo <marcwabo20@gmail.com>
2021-10-17 11:28:24 +0100

    Update config file

--- config/config.php
+++ config/config.php
@@ -1,2 +1,13 @@
 <?php
 
+//Api configuration
+define ('URL', '');
+define ('API_KEY', '');
+define ('API_AUTH_OKEN', '');
+
+//Global App configuration
+define('APP_ROOT', $_SERVER['DOCUMENT_ROOT'].'/simple-languagetool-client/');
+define ('APP_NAME', 'Language tool web client');
+define ('DB_NAME', '');
+define ('DB_USER_NAME', '');
+define ('DB_PASSWORD', '');
