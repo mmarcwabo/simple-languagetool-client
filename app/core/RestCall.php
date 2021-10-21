eabb424b4bab4394a31de2f4ceafd27fdbb38943
marcwabo <marcwabo20@gmail.com>
2021-10-21 03:31:51 +0100

    Format RestCall properly

--- app/core/RestCall.php
+++ app/core/RestCall.php
@@ -11,69 +11,6 @@ class RestCall
     //Response
     public $response;
 
-    /**
-     * @param
-     * @return json encoded responses
-     */
-    public function rest_call()
-    {
-
-        $curl = curl_init();
-
-        //If we had a token, ie premium subs
-        /**
-         * if ($token){
-         * curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-         * 'Authorization: '.$token));
-         * }
-         */
-
-        //POST, GET
-
-        switch ($this->method) {
-                //POST
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
-                if ($this->data) {
-                    if ($this->contentType) {
-                        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-                            'Content-Type: ' . $this->contentType
-                        ));
-                    }
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $this->data);
-                }
-                break;
-            case "GET":
-                //curl_setopt($curl, CURLO)
-                break;
-            default:
-                if ($this->data) {
-                    //To do - Don't hardcode API method names
-                    //Add an array of all of them
-                    //We assume we only have access on check
-                    $url = sprintf("%s?%s", URL . '/check', http_build_query($this->data));
-                }
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-                $response = curl_exec($curl);
-                curl_close($curl);
-                return $response;
-        }
-    }
-    /**
-     * data is an assoc array
-     */
-    public function post_data()
-    {
-
-        $postData = $this->data;
-        $string = http_build_query($postData);
-        //Get json from
-        $jsonResponse = $this->rest_call($this->method, URL, $string, 'application/x-xxx-form-urlencoded');
-        //return $response = json_decode($jsonResponse);
-    }
-
     public function check($language, $text)
     {
         //Get submitted data on key up
